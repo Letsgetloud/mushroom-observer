Letsgetloud/mushroom-observer

commit bc7172a4aeefaab5647aec7068161d8903f64e1c

Fix StringConcatentation in PatternSearch/Term

app/classes/pattern_search/term.rb
@@ -29,7 +29,7 @@ module PatternSearch
 
     def quote(val)
       if /['" \\]/.match?(val.to_s)
-        %Q("#{val.to_s.gsub(/(['"\\])/) { |v| '\\' + v }}")
+        %Q("#{val.to_s.gsub(/(['"\\])/) { |v| "\\{v}" }}")
      else
         val.to_s
       end

commit 444e232f3ab679f7637f6781ce94ce641e992db1

Fix typo introduced in commit bc7172a
See https://travis-ci.org/github/MushroomObserver/mushroom-observer/builds/732808679

app/classes/pattern_search/term.rb
@@ -29,7 +29,7 @@ module PatternSearch
 
     def quote(val)
       if /['" \\]/.match?(val.to_s)
-        %Q("#{val.to_s.gsub(/(['"\\])/) { |v| "\\{v}" }}")
+        %Q("#{val.to_s.gsub(/(['"\\])/) { |v| "\\#{v}" }}")
      else
         val.to_s
       end