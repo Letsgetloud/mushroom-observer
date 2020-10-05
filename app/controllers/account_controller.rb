Letsgetloud/mushroom-observer

commit 428ce16b07b5039fe079211b7fd1a2d08b7d314b

Fix AccountController StringConcatenation

app/controllers/account_controller.rb
@@ -410,7 +410,7 @@ class AccountController < ApplicationController
       # Check if we need to upload an image.
       upload = params["user"]["upload_image"]
       if upload.present?
-        date = Date.parse(params["date"]["copyright_year"].to_s + "0101")
+        date = "#{Date.parse(params["date"]["copyright_year"]}0101"
         license = License.safe_find(params["upload"]["license_id"])
         holder = params["copyright_holder"]
         image = Image.new(

app/classes/textile.rb
@@ -240,7 +240,7 @@ class Textile < String
   # This will be sent to lookup_name.
   def expand_genus_abbreviation(str)
     str.sub(/^([A-Z])\.? +(?=["a-z])/) do |x|
-      (n = @@name_lookup[Regexp.last_match(1)]) ? n + " " : x
+      (n = @@name_lookup[Regexp.last_match(1)]) ? "#{n} " : x
     end
   end
 
@@ -373,9 +373,9 @@ class Textile < String
     return url unless truncate_link_label?(url)
 
     if url =~ %r{^(\w+://[^/]+)(.*?)$}
-      Regexp.last_match(1) + "/..."
+      "#{Regexp.last_match(1)}/..."
     else
-      url[0..URL_TRUNCATION_LENGTH] + "..."
+      "#{url[0..URL_TRUNCATION_LENGTH]}..."
     end
   end
 

commit 3e944ab8b1271fccc12ae9df735dae7c9e2c3495

Fix typo introduced in commit 428ce16

app/controllers/account_controller.rb
@@ -410,7 +410,7 @@ class AccountController < ApplicationController
       # Check if we need to upload an image.
       upload = params["user"]["upload_image"]
       if upload.present?
-        date = "#{Date.parse(params["date"]["copyright_year"]}0101"
+        date = Date.parse("#{params["date"]["copyright_year"]}0101")
         license = License.safe_find(params["upload"]["license_id"])
         holder = params["copyright_holder"]
         image = Image.new(